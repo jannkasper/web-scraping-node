983f02ada9ed6aff9a86677657a21870fccefe9f
jannkasper <jann.kasper@gmail.com>
2020-11-10 07:55:54 +0100

    cheerio > extract single object

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio');
 
 const getContent = async () => {
     try {
-        const {data} = await axios.get('https://suchen.mobile.de/fahrzeuge/search.html?dam=1&isSearchRequest=true&lang=en&ms=1100%3B%3B%3B%3B&s=Car&sfmr=false&vc=Car');
+        const {data} = await axios.get('https://suchen.mobile.de/fahrzeuge/details.html?id=311219834&damageUnrepaired=NO_DAMAGE_UNREPAIRED&isSearchRequest=true&pageNumber=1&scopeId=C&sfmr=false&action=eyeCatcher&fnai=prev&searchId=c243f89d-f004-4f5c-5690-1afa9a106935&lang=en');
         const $ = await cheerio.load(data);
 
         let resultList = $('body > .viewport > div > div > .g-row > .g-col-9 > .cBox--resultList > .cBox-body--resultitem');
@@ -102,4 +102,69 @@ const getContent = async () => {
         throw e;
     }
 };
-getContent();
+
+
+const getSingleElement = async () => {
+    const {data} = await axios.get('https://suchen.mobile.de/fahrzeuge/details.html?id=310711740&damageUnrepaired=NO_DAMAGE_UNREPAIRED&isSearchRequest=true&makeModelVariant1.makeId=3500&pageNumber=2&scopeId=C&sfmr=false&searchId=8b7aa788-479d-e66f-286b-734d69814795&lang=en');
+    const $ = await cheerio.load(data);
+
+
+    // let result = $('body > .viewport > div > div:nth-child(2)').children().each((index,elem) => {console.log($(elem).get(0).tagName)});
+
+    // <div class="g-col-8">
+    let mainBox = $('body > .viewport > div > div ').not('.adv, .sky08').children().last().children().first();
+
+    console.log('------------------------------------------------');
+    if (mainBox.children().hasClass('cBox--vehicle-details')) {
+        // let next = $('.cBox--vehicle-details', mainBox).each((index, elem) => {console.log($(elem).get(0).tagName, ' ', $(elem).attr())});
+
+        // let title = $('.cBox--vehicle-details > .cBox-body--title-area > div > div > #rbt-ad-title', mainBox).text();
+        // console.log(title);
+        //
+        // let images = [];
+        // let imageGallery = $('.cBox--vehicle-details > .cBox-body > div > div > .data-area-image-gallery > .slick-image-gallery-wrapper > .image-gallery-wrapper > .image-gallery > .gallery', mainBox).children().first();
+        // let getImages = $('div', imageGallery).children().filter((index, elem) => $(elem).get(0).tagName == 'img').each((index,elem) => images.push($(elem).attr('data-lazy')));
+        // console.log(images);
+        //
+        // let technicalData = [];
+        // let technicalSource = $('.cBox--content > .cBox-body--technical-data', mainBox);
+        // let getPrice = $('.u-margin-bottom-9', technicalSource).first()
+        //         .each((index, elem) => {
+        //             let key = $('div:nth-child(1)', elem).text();
+        //             let value = $('div:nth-child(2) > span', elem).text();
+        //             key && value ? technicalData.push([key, value]) : null;
+        //         });
+        // let getData = $('.u-margin-bottom-9', technicalSource)
+        //     .filter((index,elem) => $('div:nth-child(2)', elem).children().length === 0)
+        //     .each((index, elem) => {
+        //         let key = $('div:nth-child(1)', elem).text();
+        //         let value = $('div:nth-child(2)', elem).text();
+        //         key && value ? technicalData.push([key, value]) : null;
+        //     });
+        // console.log(technicalData);
+        //
+        // let featuresData = [];
+        // let featuresSource = $('.cBox--content > #rbt-features > div > div > div', mainBox).each((index,elem) => featuresData.push($(elem).text()));
+        // console.log(featuresData);
+        //
+        //
+        // let descriptionSource = $('.cBox--content > .cBox-body--vehicledescription > div > .description', mainBox);
+        // let vehicleDescription = descriptionSource.html().replace(/<br ?\/?>/g, "\n");
+        // console.log(vehicleDescription);
+
+        // let mainBox = $('head > script ').each((index, elem) => {console.log($(elem).get(0).tagName, ' ', $(elem).text())});
+
+        let scriptText = $('head > script')
+            .filter((index, elem) => $(elem).get()[0].children[0] && ($(elem).get()[0].children[0].data).includes('setAdData'))
+            .map((index, elem) => $(elem).get()[0].children[0].data).get(0);
+
+        let scriptString = scriptText.split('({')[1].split('})')[0];
+        var obj = JSON.parse('{' +  scriptString + '}');
+        console.log(obj);
+
+    } else {
+        getSingleElement();
+    }
+}
+
+getSingleElement();
